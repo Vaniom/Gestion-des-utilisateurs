5d33475e209899149084f6540bacfac8b5d45519
Vaniom <30296608+Vaniom@users.noreply.github.com>
2018-02-05 14:37:47 +0000

    Update connexion.php
    Code de vérification des données utilisateur, gestion de l'option "remember me" et création d'un cookie.

--- connexion.php
+++ connexion.php
@@ -9,12 +9,12 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 //
-//$pass_hache = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+$pass_hache = password_hash($_POST['pass'], PASSWORD_DEFAULT);
 $pseudo = ($_POST['pseudo']);
 $pass = $_POST['pass'];
 
 // Vérification des identifiants
-$req = $bdd->prepare('SELECT * FROM membres WHERE pseudo = :pseudo/* AND pass = :pass*/');
+$req = $bdd->prepare('SELECT * FROM membres WHERE pseudo = :pseudo');
 $req->execute(array(
     'pseudo' => $pseudo));
 
@@ -28,10 +28,26 @@ if (password_verify($pass, $hache))
     session_start();
     $_SESSION['id'] = $resultat['id'];
     $_SESSION['pseudo'] = $pseudo;
-    echo "Bienvenue $pseudo !<br/>Retour à <a href='index.html'>l'accueil</a>";
+    $_SESSION['pass'] = password_hash($pass, PASSWORD_DEFAULT);
+
+    //gestion de l'option "remember me" en ecrivant un cookie
+
+    			if(isset($_POST['remember'])){
+				setcookie("pseudo", $pseudo, time() + 365 * 24 * 60 * 60, null, null, false, true);
+				setcookie("pass", $hache, time() + 365 * 24 * 60 * 60, null, null, false, true);
+			}else{
+				if(ISSET($_COOKIE['pseudo'])){
+					setcookie("pseudo", "");
+				}
+				if(ISSET($_COOKIE['pass'])){
+					setcookie("pass", "");
+				}
+			}
+
+    echo "<br/>Bienvenue"; echo $pseudo; echo "!<br/>Retour à <a href='index.php'>l'accueil</a>";
 }
 else
 {
-    echo "Le login ou le mot de passe sont incorrects :(<br/>Retour à <a href='index.html'>l'accueil</a>";
+    echo "Le login ou le mot de passe sont incorrects :(<br/>Retour à <a href='index.php'>l'accueil</a>";
 }
 ?>
